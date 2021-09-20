522e7fc60babd1e1125a2a38739aeaf87eff2998
jesseburke <jtb445@gmail.com>
2021-09-20 15:40:57 +1000

    renamed some packages
    data => jotai-data-setup
    three-scene-in-react => three-scene-with-react
    svg-scene-in-react => svg-scene-with-react

--- geometries/IntegralCurveGeom.js
+++ geometries/IntegralCurveGeom.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 
-import { RK4Pts } from '@jesseburke/math-utils';
+import { RK4Pts } from '@jesseburke/math';
 import CurvedPathGeom from './CurvedPathGeom';
 
 // function func is a function of x and y
